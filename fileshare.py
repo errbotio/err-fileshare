f4e3fe603748c1683740e9129187007d0349c056
Sijis Aviles <sijis.aviles+github@gmail.com>
2021-07-27 01:39:42 -0500

    style: black and isort file

--- fileshare.py
+++ fileshare.py
@@ -1,9 +1,10 @@
 import logging
-from errbot import botcmd, BotPlugin
-from config import BOT_DATA_DIR
-from os import path, mkdir, walk
-from io import open
 import shutil
+from io import open
+from os import mkdir, path, walk
+
+from config import BOT_DATA_DIR
+from errbot import BotPlugin, botcmd
 
 FILESHARE_PATH = path.join(BOT_DATA_DIR, "public")
 
@@ -12,13 +13,12 @@ if not path.exists(FILESHARE_PATH):
 
 
 class FileShare(BotPlugin):
-    min_err_version = '2.2.0-beta' # for file transfers
 
     def target(self, name):
         full_path = path.join(FILESHARE_PATH, name)
         if full_path != path.abspath(full_path):
             logging.warn('Refused the filename "%s" is it an injection attempt?' % name)
-            return ''
+            return ""
         return full_path
 
     def callback_stream(self, stream):
@@ -35,7 +35,6 @@ class FileShare(BotPlugin):
             shutil.copyfileobj(stream, destination)
         self.send(stream.identity, "File %q well received." % stream.name)
 
-
     @botcmd
     def download(self, mess, args):
         target = self.target(args)
@@ -43,10 +42,14 @@ class FileShare(BotPlugin):
             return 'Invalid filename "%s"' % target
 
         if not path.exists(target):
-            return 'File not found %s' % args
-        self.send_stream_request(mess.frm, open(target, 'rb'), name=args, size=path.getsize(target))
-        return 'File request sent'
+            return "File not found %s" % args
+        self.send_stream_request(
+            mess.frm, open(target, "rb"), name=args, size=path.getsize(target)
+        )
+        return "File request sent"
 
     @botcmd
     def ls(self, mess, args):
-        return '\n'.join(['\n'.join([n for n in f]) for p, _, f in walk(FILESHARE_PATH)])
+        return "\n".join(
+            ["\n".join([n for n in f]) for p, _, f in walk(FILESHARE_PATH)]
+        )
