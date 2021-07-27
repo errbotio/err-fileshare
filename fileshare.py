9d40496c2a24ae1be4157ee00d89ba0d0e7f1d43
Sijis Aviles <sijis.aviles+github@gmail.com>
2021-07-27 01:58:41 -0500

    fix: logging and string format

--- fileshare.py
+++ fileshare.py
@@ -11,38 +11,39 @@ FILESHARE_PATH = path.join(BOT_DATA_DIR, "public")
 if not path.exists(FILESHARE_PATH):
     mkdir(FILESHARE_PATH)
 
+LOG = logging.getLogger("errbot.plugin.fileshare")
+
 
 class FileShare(BotPlugin):
-
     def target(self, name):
         full_path = path.join(FILESHARE_PATH, name)
         if full_path != path.abspath(full_path):
-            logging.warn('Refused the filename "%s" is it an injection attempt?' % name)
+            LOG.warn("Refused the filename '%s' is it an injection attempt?", name)
             return ""
         return full_path
 
     def callback_stream(self, stream):
         if not stream.name:
-            logging.info("Anonymous stream, I can't save that")
+            LOG.info("Anonymous stream, I can't save that")
             return
 
-        logging.debug('Receive the file "%s"' % stream.name)
+        LOG.debug("Receive the file '%s'", stream.name)
         destination_path = self.target(stream.name)
         if not destination_path:
-            self.send(stream.identity, "Invalid filename %s." % stream.name)
+            self.send(stream.identity, f"Invalid filename {stream.name}.")
             return
         with open(destination_path, "wb") as destination:
             shutil.copyfileobj(stream, destination)
-        self.send(stream.identity, "File %q well received." % stream.name)
+        self.send(stream.identity, f"File {stream.name} well received.")
 
     @botcmd
     def download(self, mess, args):
         target = self.target(args)
         if not target:
-            return 'Invalid filename "%s"' % target
+            return f"Invalid filename '{target}'"
 
         if not path.exists(target):
-            return "File not found %s" % args
+            return f"File not found {args}"
         self.send_stream_request(
             mess.frm, open(target, "rb"), name=args, size=path.getsize(target)
         )
